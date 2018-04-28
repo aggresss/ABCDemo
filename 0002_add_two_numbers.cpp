59f1bf3356775247808aee3bd71da4974b36153c
Jagger Yu <aggresss@163.com>
2018-04-28 17:15:42 +0800

    Update 0002_add_two_numbers.cpp

--- 0002_add_two_numbers.cpp
+++ 0002_add_two_numbers.cpp
@@ -14,7 +14,7 @@
  * };
  */
  
-class Solution {
+class Solution1 {
 public: 
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         if (l1 == NULL || l2 == NULL) {
@@ -27,5 +27,44 @@ public:
             result->next = addTwoNumbers(new ListNode(value / 10), result->next);
         }
         return result;
+    }
+};
+
+class Solution2 {
+public:
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode preHead(0), *p = &preHead;
+    int carry = 0;
+    while (l1 || l2 || carry) {
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        carry = sum / 10;
+        p->next = new ListNode(sum % 10);
+        p = p->next;
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+    }
+    return preHead.next;
+};
+ 
+ class Solution3 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    	ListNode* head = new ListNode(-1);
+    	ListNode* p = head;
+    	ListNode* p1 = l1;
+    	ListNode* p2 = l2;
+    	int c = 0, val1, val2, sum;
+        while(p1 || p2 || c != 0){
+        	val1 = p1 == NULL ? 0 : p1->val;
+        	val2 = p2 == NULL ? 0 : p2->val;
+        	sum = val1 + val2 + c;
+        	c = sum/10;
+        	ListNode* node = new ListNode(sum%10);
+        	p->next = node;
+        	p = node;
+        	p1 = p1 != NULL ? p1->next : NULL;
+        	p2 = p2 != NULL ? p2->next : NULL;
+        }
+        return head->next;
     }
 };
