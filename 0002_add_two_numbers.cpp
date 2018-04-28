f5676728d22df1606925b25ecfc5b4bf6ec56cf3
Jagger Yu <aggresss@163.com>
2018-04-28 17:33:44 +0800

    Update 0002_add_two_numbers.cpp

--- 0002_add_two_numbers.cpp
+++ 0002_add_two_numbers.cpp
@@ -32,16 +32,17 @@ public:
 
 class Solution2 {
 public:
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode preHead(0), *p = &preHead;
-    int carry = 0;
-    while (l1 || l2 || carry) {
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-        carry = sum / 10;
-        p->next = new ListNode(sum % 10);
-        p = p->next;
-        l1 = l1 ? l1->next : l1;
-        l2 = l2 ? l2->next : l2;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode preHead(0), *p = &preHead;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
+        }
+        return preHead.next;
     }
-    return preHead.next;
 };
