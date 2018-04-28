efa885a3eb98458c2fbd87790601e23f73a061f7
Jagger Yu <aggresss@163.com>
2018-04-28 13:35:14 +0800

    Update 0001_two_sum.cpp

--- 0001_two_sum.cpp
+++ 0001_two_sum.cpp
@@ -6,15 +6,15 @@
 // class Solution {
 // public:
 //     vector<int> twoSum(vector<int>& nums, int target) {
-//     	vector<int> res;
+//         vector<int> res;
 //         for(int i = 0; i < nums.size(); i++){
-//         	for(int j = i+1; j < nums.size(); j++){
-//         		if(nums[i] + nums[j] == target && i != j){
-//         			res.push_back(i);
-//         			res.push_back(j);
-//         			return res;
-//         		}
-//         	}
+//             for(int j = i+1; j < nums.size(); j++){
+//                 if(nums[i] + nums[j] == target && i != j){
+//         		       res.push_back(i);
+//         			   res.push_back(j);
+//         			   return res;
+//                 }
+//         	   }
 //         }
 //         return res;
 //     }
