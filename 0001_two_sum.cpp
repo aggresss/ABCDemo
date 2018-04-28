4a109640e6e2f9bd5b9e684914058d9b4557c4ae
Jagger Yu <aggresss@163.com>
2018-04-28 13:37:42 +0800

    Update 0001_two_sum.cpp

--- 0001_two_sum.cpp
+++ 0001_two_sum.cpp
@@ -3,24 +3,25 @@
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  */
 
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> res;
-//         for(int i = 0; i < nums.size(); i++){
-//             for(int j = i+1; j < nums.size(); j++){
-//                 if(nums[i] + nums[j] == target && i != j){
-//         		       res.push_back(i);
-//         			   res.push_back(j);
-//         			   return res;
-//                 }
-//         	   }
-//         }
-//         return res;
-//     }
-// };
+class Solution1 {
+  public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
 
-class Solution {
+class Solution2 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> v(2, 0);
