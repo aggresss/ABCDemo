b197b8a4e0c6da98204d96dcce27472e7a7f133e
Jagger Yu <aggresss@163.com>
2018-04-30 17:02:15 +0800

    Update 0020_valid_parentheses.cpp

--- 0020_valid_parentheses.cpp
+++ 0020_valid_parentheses.cpp
@@ -6,24 +6,24 @@
 *     Open brackets must be closed in the correct order.
 */
 
-class Solution {  
-public:  
-    bool isValid(string s) {  
-        stack<char> result;  
-        int n=s.size();  
-        if(n==0) return true;  
-     for(int i=0;i<n;i++)  
-            {  
-                if(result.empty())  
-                    result.push(s[i]);  
-                else if(result.top()=='('&&s[i]==')'||  
-                      result.top()=='['&&s[i]==']'||  
-                      result.top()=='{'&&s[i]=='}')   
-                        result.pop();  
-                else  
-                    result.push(s[i]);  
-                  
-            }  
-            return result.empty();  
-    }  
-};  
+class Solution
+{
+  public:
+    bool isValid(string s) {
+        stack<char> result;
+        int n = s.size();
+        if (n == 0)
+            return true;
+        for (int i = 0; i < n; i++) {
+            if (result.empty())
+                result.push(s[i]);
+            else if (result.top() == '(' && s[i] == ')' ||
+                     result.top() == '[' && s[i] == ']' ||
+                     result.top() == '{' && s[i] == '}')
+                result.pop();
+            else
+                result.push(s[i]);
+        }
+        return result.empty();
+    }
+};
