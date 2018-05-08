bb424a582211eedaec8c5cfbd8e6fc0a3460aebe
Jagger Yu <aggresss@163.com>
2018-05-09 07:50:20 +0800

    Update 0094_binary_tree_inorder_traversal.cpp

--- 0094_binary_tree_inorder_traversal.cpp
+++ 0094_binary_tree_inorder_traversal.cpp
@@ -1,3 +1,28 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+       vector<int> result;
+        stack< TreeNode *> node_stack;
+        TreeNode * pNode = root;
+        while (pNode || !node_stack.empty()) {
+             //节点不为空，加入栈中，并访问节点左子树
+             if (pNode != NULL) {
+                node_stack.push(pNode);
+                pNode = pNode->left;
+            }else{
+             //节点为空，从栈中弹出一个节点，访问这个节点，
+                pNode = node_stack.top();
+                node_stack.pop();
+              //访问节点右子树
+                result.push_back(pNode->val);
+                pNode = pNode->right;
+            }
+        }
+        return result; 
+    }
+};
+
+
 //包裹结构体，标明左子树是否已经访问过
  struct TreeNodeWrapper{
     struct TreeNode *pNode;
